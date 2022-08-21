df29030673b663bcab72046593ef925830a5cd10
Жуков Виталий <zhuksa1@yandex.ru>
2022-08-21 18:08:07 +0300

    Социальная сеть_005

--- yatube/posts/tests/test_forms.py
+++ yatube/posts/tests/test_forms.py
@@ -102,6 +102,7 @@ class PostFormTests(TestCase):
             ).exists()
         )
 
+
 def test_update_post_edit(self):
     """
     При редактировании поста через форму изменяется соответствующая запись
