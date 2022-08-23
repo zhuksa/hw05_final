e6de5fb4b042680c1cffed121958278806969920
Жуков Виталий <zhuksa1@yandex.ru>
2022-08-24 01:17:49 +0300

    Социальная сеть_064

--- yatube/posts/admin.py
+++ yatube/posts/admin.py
@@ -17,6 +17,7 @@ class PostAdmin(admin.ModelAdmin):
     list_filter = ('pub_date',)
     empty_value_display = '-пусто-'
 
+
 @admin.register(Group)
 class GroupAdmin(admin.ModelAdmin):
     list_display = ('pk', 'title', 'slug', 'description')
